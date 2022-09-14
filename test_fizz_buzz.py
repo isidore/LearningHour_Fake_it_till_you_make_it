73c3ec0836ffa33538166e2aa6cc2fd3756194ea
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-09-14 20:39:12 +0000

    extract

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -1,8 +1,8 @@
 def fizz_buzz(param):
     #list= [1]
     i = 1
-
-    return f"{i}," + f"{2},"+"Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32"
+    i1 = 2
+    return f"{i}," + f"{i1}," + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32"
 
 
 def test_fizz_buzz():
