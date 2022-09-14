61d9e889521f0b684d14e8acbb778bbfd69f277d
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-09-14 20:30:06 +0000

    separate

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -1,5 +1,5 @@
 def fizz_buzz(param):
-    return "1"+",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32"
+    return "1,"+"2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32"
 
 
 def test_fizz_buzz():
