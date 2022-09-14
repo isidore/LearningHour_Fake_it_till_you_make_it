6a5247c44c39bd04a3a61e1f851d5a41f8cac363
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-09-14 21:29:00 +0000

    automate

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -1,9 +1,9 @@
 def fizz_buzz(param):
     fizzbuzz_string = ""
-    list = range(1,16)
+    list = range(1,17)
     for j in list:
         fizzbuzz_string += calculate_number(j)
-    return fizzbuzz_string + "16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32"
+    return fizzbuzz_string + "17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32"
 
 
 def calculate_number(number):
