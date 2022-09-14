1910330deb90e50355593860fabf8cc01153adc1
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-09-14 20:45:22 +0000

    calculate

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -3,7 +3,7 @@ def fizz_buzz(param):
     i = 1
     i1 = 2
 
-    fizzbuzz_string = calculate_number(i) + f"{i1},"
+    fizzbuzz_string = calculate_number(i) + calculate_number(i1)
     return fizzbuzz_string + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32"
 
 
